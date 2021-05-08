2f775774e3cf0c6bcb362dd90cd835b75e462988
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2021-05-08 21:57:08 +0200

    add an empty project

---
+++ SwiftUI_exercice1/SwiftUI_exercice1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_exercice1
+//
+//  Created by Thierry Vilmart on 2021-05-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_exercice1/SwiftUI_exercice1/SwiftUI_exercice1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_exercice1App.swift
+//  SwiftUI_exercice1
+//
+//  Created by Thierry Vilmart on 2021-05-08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_exercice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
